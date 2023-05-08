6128d89e2983bc8ff566acf6191514e89554cd6f
Daniel Agg <daniel.agg@outlook.com>
2023-05-08 21:15:41 +0200

    dataManager example

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace c_sharp_vs_rust_article
+{
+    class DataManager
+    {
+        private readonly ImmutableList<int> myPrivateData;
+        public ImmutableList<int> GetData() => myPrivateData;
+
+        public DataManager()
+        {
+            myPrivateData = ImmutableList.Create<int>(1, 2, 3, 4, 5);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var dataManager = new DataManager();
+            var data = dataManager.GetData();
+
+            // here we do some processing with the data
+            data.Add(6);
+            data = data.Skip(2).Take(4).ToImmutableList();
+
+            Console.WriteLine(string.Join(", ", data));
+            Console.WriteLine(string.Join(", ", dataManager.GetData()));
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+struct DataManager {
+    my_private_data: Vec<i32>,
+}
+
+impl Default for DataManager {
+    fn default() -> Self {
+        DataManager {
+            my_private_data: vec![1, 2, 3, 4, 5],
+        }
+    }
+}
+
+// impl DataManager {
+//     fn get_data(&self) -> &Vec<i32> {
+//         return &self.my_private_data;
+//     }
+// }
+
+impl DataManager {
+    fn get_data(&mut self) -> &mut Vec<i32> {
+        &mut self.my_private_data
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut data_manager = DataManager::default();
+
+    let mut data = data_manager.get_data();
+
+    data.push(6);
+    let data: Vec<&mut i32> = data.into_iter().skip(2).take(4).collect();
+
+    println!(
+        "{}",
+        data.iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+            .join(", ")
+    );
+
+    println!(
+        "{}",
+        data_manager
+            .get_data()
+            .iter()
+            .map(|&x| x.to_string())
+            .collect::<Vec<String>>()
+            .join(", ")
+    );
 }
